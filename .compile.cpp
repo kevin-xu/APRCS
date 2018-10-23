5ce4a5bd05454309fd8e670092f01722e0016037
Kevin Xu <kevin.xu.1982.02.06@gmail.com>
2018-10-23 12:15:19 +0800

    Add files via upload

---
+++ .compile.cpp
@@ -0,0 +1,14 @@
+/*
+ *
+ * Author: Kevin XU <kevin.xu.1982.02.06@gmail.com>
+ *
+ */
+
+#include <auto-ptr.hpp>
+#include <ref-counting.hpp>
+#include <signaling.hpp>
+
+
+
+// TODO: Need to instantiate all templates, so as to compile them completely.
+
